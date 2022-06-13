1eb204a7c6902fd5ffd24b0a44fed8a653e2db54
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-06-13 21:06:50 +0200

    feat/navbar_button_and_links

--- src/components/Navigation/LoggedOut.js
+++ src/components/Navigation/LoggedOut.js
@@ -1,10 +1,29 @@
+import { createTheme, Link } from "@mui/material";
 import React from "react";
-import NavbarItem from "./NavbarItem";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#ff9800",
+      main: "#fb8c00",
+      dark: "#e65100",
+      contrastText: "#fff",
+    },
+  },
+});
 
 export default function LoggedOut() {
   return (
     <>
-      <NavbarItem path="/login" linkText="Login" />
+      <Link
+        href="/login"
+        color="inherit"
+        underline="none"
+        theme={theme}
+        className="custom-link"
+      >
+        LOGIN
+      </Link>
     </>
   );
 }
