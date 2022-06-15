1bb2760276c3dd233c05b9a4dbf42d6dc3039576
naomi-eliasar <nreliasar@gmail.com>
2022-06-15 12:40:45 +0200

    /fix show member names

--- src/pages/Club/index.js
+++ src/pages/Club/index.js
@@ -2,7 +2,9 @@ import "./style.css";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { selectClubs } from "../../store/club/selector";
+import { Grid, Modal, Button, Typography, Box, Divider } from "@mui/material";
+
+import { selectClubs, selectClubMembers } from "../../store/club/selector";
 import { fetchClub } from "../../store/club/thunk";
 import { Loading } from "../../components";
 
@@ -10,6 +12,8 @@ const Club = () => {
   const dispatch = useDispatch();
   const routeParams = useParams();
   const clubDetails = useSelector(selectClubs);
+  const clubMembers = useSelector(selectClubMembers);
+  console.log(clubMembers);
 
   useEffect(() => {
     dispatch(fetchClub(routeParams.id));
@@ -17,12 +21,39 @@ const Club = () => {
 
   return clubDetails ? (
     <div className="clubpage">
-      {clubDetails.name}
-      {clubDetails.description}
+      <div className="clubbanner">
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          pl={2}
+          style={{
+            backgroundColor: `${clubDetails.backgroundcolor}`,
+            color: `${clubDetails.textcolor}`,
+            minHeight: "200px",
+          }}
+        >
+          <Grid item xs={4}>
+            <img
+              alt="club"
+              src={
+                clubDetails.pictureUrl
+                  ? clubDetails.pictureUrl
+                  : "https://img.gamewith.net/img/bcfc573ae2779a1eec9ad044f7afd037.jpg"
+              }
+            />
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={7}>
+            <h1>{clubDetails.name}</h1>
+            <h5>"{clubDetails.description}"</h5>
+          </Grid>
+        </Grid>
+      </div>
       <div className="clubmembers">
         Members:
-        {clubDetails.users.map((user) => {
-          return user.name;
+        {clubMembers.map((member) => {
+          return member.name;
         })}
       </div>
     </div>
